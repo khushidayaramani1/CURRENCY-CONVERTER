0dda3466f9c36d60c5c0ebab843254d79135d113
Khushi Dayaramani <khushi.dayaramani1@gmail.com>
2024-12-20 17:24:11 +0530

    Add files via upload

---
+++ CURRENCY_CONVERTER/script.js
@@ -0,0 +1,7 @@
+const url="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+
+const from=document.querySelectorAll(".from-to select option");
+ 
+const updateFlag(){
+    
+}
